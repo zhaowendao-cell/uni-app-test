ab0eb007e26986d5b392732f11450d3f3d91b131
史鹏琦 <315468902@qq.com>
2026-08-05 21:12:27 +0800

    Add files via upload

---
+++ components/common-title/common-title.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="common-title">
+		
+		<view class="title">公共标题</view>
+		
+		<view class="more">更多</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"common-title",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+.common-title{
+	display: flex;
+	height: 30rpx;
+	.title{
+		width: 200rpx;
+	}
+	.more{
+		width: 100rpx;
+	}
+
+}
+</style>
